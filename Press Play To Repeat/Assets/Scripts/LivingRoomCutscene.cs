e5709bd330044dff6b33c8b365300bb1b223c50b
Jay Miller <jayswork9@gmail.com>
2018-08-10 12:17:48 +0930

    Added cutscene and added dice into level.

---
+++ Press Play To Repeat/Assets/Scripts/LivingRoomCutscene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivingRoomCutscene : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject CutsceneCam;
+    public GameObject Dice;
+    public GameObject Trigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Player.SetActive(false);
+        CutsceneCam.SetActive(true);
+        Dice.SetActive(true);
+        StartCoroutine(EndAnim());
+    }
+
+    IEnumerator EndAnim()
+    {
+        yield return new WaitForSeconds(13.5f);
+        Player.SetActive(true);
+        CutsceneCam.SetActive(false);
+        Dice.SetActive(false);
+        Trigger.SetActive(false);
+    }
+
+}
